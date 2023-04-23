09c2d46ea2f04e7e6787b271c23bc3f13a4380f4
ianczm <ianczm@live.co.uk>
2023-04-23 19:41:16 +0800

    Add visualisation helpers

--- src/helpers/__init__.py
+++ src/helpers/__init__.py
@@ -1,3 +1,4 @@
 from .constants import *
 from .processors import *
 from .repositories import *
+from .visualisation import *
---
+++ src/helpers/visualisation.py
@@ -0,0 +1,105 @@
+import pandas as pd
+import math
+import plotly.express as px
+from plotly.subplots import make_subplots
+import plotly.graph_objects as go
+
+
+class Visualiser:
+
+    name: str
+
+    def __init__(self, name: str):
+        self.name = name
+
+    def x_name(self):
+        return self.name + '1'
+
+    def y_name(self):
+        return self.name + '2'
+
+    def generate_plot_coloured_by_features(self, dataframe: pd.DataFrame, color_by: str):
+        px.scatter(
+            dataframe,
+            x=self.x_name(),
+            y=self.y_name(),
+            hover_data=['artist', 'song_name'],
+            template='plotly_dark',
+            color=color_by
+        ).show()
+
+    def generate_hover_template(self, field_names: list[str]):
+        return '<br>'.join([f'<b>{name}</b>: %{{customdata[{idx}]}}' for idx, name in enumerate(field_names)]) + '<extra></extra>'
+
+    def generate_colour_feature_trace(self, fig: go.Figure, dataframe: pd.DataFrame, feature: str, row: int, col: int):
+        fig.add_trace(go.Scatter(
+            x=dataframe[self.x_name()],
+            y=dataframe[self.y_name()],
+            customdata=dataframe[['playlist', 'artist', 'song_name']].to_numpy(),
+            mode='markers',
+            hovertemplate=self.generate_hover_template(['playlist', 'artist', 'song_name']),
+            showlegend=False,
+            marker={'color': dataframe[feature], 'colorscale': 'Agsunset'},
+            name=feature),
+            row=row,
+            col=col
+        )
+
+    def generate_subplot_colour_all_features(self, dataframe: pd.DataFrame, features: list[str], cols: int = 3):
+        blacklisted_features = ['playlist', 'artist', 'song_name', self.x_name(), self.y_name()]
+        rows = math.ceil(len(features) / cols)
+        fig = make_subplots(rows=rows, cols=cols, subplot_titles=(
+            tuple(feature for feature in features if feature not in blacklisted_features)
+        ))
+        idx = 0
+        for feature in features:
+            if feature not in blacklisted_features:
+                self.generate_colour_feature_trace(
+                    fig,
+                    dataframe,
+                    feature,
+                    row=(math.floor(idx/cols)+1),
+                    col=(idx%cols)+1
+                )
+                idx += 1
+        fig.update_layout(title=f'{self.name} Result Coloured by Features', height=350*rows)
+        fig.update_annotations(font={'size': 12})
+        fig.show()
+
+    def generate_tsne_trace(self, fig, tsne, row, col, playlists, artists, song_names):
+        tsne['playlist'] = playlists
+        tsne['artist'] = artists
+        tsne['song_name'] = song_names
+        for pl in pd.unique(tsne['playlist']):
+            filtered_tsne = tsne[tsne['playlist'] == pl]
+            fig.add_trace(go.Scatter(
+                x=filtered_tsne[self.x_name()],
+                y=filtered_tsne[self.y_name()],
+                customdata=filtered_tsne.iloc[:, -3:].to_numpy(),
+                mode='markers',
+                hovertemplate=self.generate_hover_template(['playlist', 'artist', 'song_name']),
+                showlegend=False,
+                name=pl),
+                row=row,
+                col=col
+            )
+
+    def generate_tsne_perplexity_subplots(self, tsne_array, cols, playlists, artists, song_names):
+        rows = math.ceil(len(tsne_array) / cols)
+        fig = make_subplots(rows=rows, cols=cols, subplot_titles=(tuple(f'Perplexity: {p}, Exaggeration: {e}' for e, p, _ in tsne_array)))
+
+        for idx, item in enumerate(tsne_array):
+            ee, perplexity, tsne = item
+            self.generate_tsne_trace(
+                fig,
+                tsne,
+                row=(math.floor(idx/cols)+1),
+                col=(idx%cols)+1,
+                playlists=playlists,
+                artists=artists,
+                song_names=song_names
+            )
+
+        fig.update_layout(title=f'{self.name} Perplexities', height=350*rows)
+        fig.update_annotations(font={'size': 12})
+        fig.show()
