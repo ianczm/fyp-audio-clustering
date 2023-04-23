897772a1df1fa16777ddfbc8c5bb90f801e8c550
ianczm <ianczm@live.co.uk>
2023-04-23 19:43:38 +0800

    Add config file to notebooks folder

---
+++ src/notebooks/config.py
@@ -0,0 +1,12 @@
+extracted_dir = '../../data/extracted'
+temp_dir = '../../data/temp/2d-pca-gmm.pkl.bz2'
+result_dir = '../../data/temp/2d-pca-gmm-results.pkl.bz2'
+
+fresh_load_dataset_dir = '../../data/temp/0-fresh-load.pkl.bz2'
+visualisation_result_dir = '../../data/temp/1-visualisation.pkl.bz2'
+dim_reduction_result_dir = '../../data/temp/2-dim-reduction.pkl.bz2'
+pca_result_dir = '../../data/temp/3-pca.pkl.bz2'
+tsne_result_dir = '../../data/temp/3-tsne.pkl.bz2'
+
+metadata_columns = ['song_name', 'artist', 'playlist']
+
